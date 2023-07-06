b95828cb04f9cbad78353bfb5de56d198beeeb71
Morgan <morgan.espindola@gmail.com>
2023-07-06 19:45:52 -0300

    Section 15: Preparation finished

--- 015-react-sass/src/App.jsx
+++ 015-react-sass/src/App.jsx
@@ -1,7 +1,13 @@
+import MainContent from "./components/MainContent";
+import Sidebar from "./components/Sidebar";
+import "./styles/components/app.scss";
+
 function App() {
   return (
-    <div className="App">
-      <h1>React + SASS</h1>
+    <div id="portfolio">
+      <h1>Morgan Espindola</h1>
+      <Sidebar />
+      <MainContent />
     </div>
   );
 }
