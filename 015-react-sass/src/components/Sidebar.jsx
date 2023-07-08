b906658e6f3a7f194bd0175a4f13017b21d60224
Morgan <morgan.espindola@gmail.com>
2023-07-07 23:30:22 -0300

    Section 15: Sidebar Component

--- 015-react-sass/src/components/Sidebar.jsx
+++ 015-react-sass/src/components/Sidebar.jsx
@@ -1,5 +1,18 @@
+import Avatar from "../img/me.jpg";
+import "../styles/components/sidebar.scss";
+
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <aside id="sidebar">
+      <img src={Avatar} alt="Morgan Espindola" />
+      <div className="title">Software Engineer</div>
+      <p>social media</p>
+      <p>contact</p>
+      <a href="" className="btn">
+        Download CV
+      </a>
+    </aside>
+  );
 };
 
 export default Sidebar;
