125da3e65dfd4cbaf209cdd4b2c6e0e2eae3da6d
Morgan <morgan.espindola@gmail.com>
2023-07-11 18:35:58 -0300

    Section 15: Finished ProjectsContainer

--- 015-react-sass/src/components/ProjectsContainer.jsx
+++ 015-react-sass/src/components/ProjectsContainer.jsx
@@ -1,5 +1,18 @@
 const ProjectsContainer = () => {
-  return <div>ProjectsContainer</div>;
+  return (
+    <section className="projects-container">
+      <h2>Projects</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima deleniti
+        expedita dicta laboriosam, laborum a dolore temporibus error? Molestiae
+        eligendi quidem omnis a cupiditate maiores itaque, necessitatibus animi
+        unde quas!
+      </p>
+      <a href="#" className="btn">
+        See my projects
+      </a>
+    </section>
+  );
 };
 
 export default ProjectsContainer;
